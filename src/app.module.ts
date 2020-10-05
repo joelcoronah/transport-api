5b205fa7c037fbe32638064399d25ce2bc0313b8
joelcoronah <joelcoronah@gmail.com>
2020-10-05 00:20:25 -0400

    :sparkles: feat(db): add config file, .env and database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: false,
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DATABASE_TYPE'),
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USERNAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
+          entities: [configService.get('TYPEORM_ENTITIES')],
+          subscribers: [configService.get('TYPEORM_SUBSCRIBERS')],
+          synchronize: false,
+        } as TypeOrmModuleOptions;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
